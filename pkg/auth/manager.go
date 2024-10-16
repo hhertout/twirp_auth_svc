5f36a11f983ee7cd01879391ee30c0c14e525652
hhertout <hugues.hertout@gmail.com>
2024-10-16 21:49:42 +0200

    doc: improve doc about auth manager

--- pkg/auth/manager.go
+++ pkg/auth/manager.go
@@ -10,8 +10,35 @@ import (
 	"github.com/hhertout/twirp_auth/pkg/tools"
 )
 
+// AuthManagerInterface defines the methods required for managing authentication and authorization.
+// It includes methods for restricting and allowing access based on user roles.
 type AuthManagerInterface interface {
+	// RestrictAccessWithRole restricts access to a user based on their role.
+	// It verifies the JWT token from the context and checks if the user has one of the required roles.
+	// If the token is missing, invalid, or the user does not have the required role, it returns an error.
+	// It return an error if the user have the role specified in the slice or the user otherwise.
+	//
+	// Parameters:
+	// - ctx: the context containing the JWT token.
+	// - roles: a slice of roles that are required for access.
+	//
+	// Returns:
+	// - The user if they have the required role.
+	// - An error if the token is missing, invalid, or the user does not have the required role.
 	RestrictAccessWithRole(ctx context.Context, roles []role.ROLE) (repository.User, error)
+
+	// AllowAccessWithRole allows access to a user based on their role.
+	// It verifies the JWT token from the context and checks if the user has one of the required roles.
+	// If the token is missing, invalid, or the user does not have the required role, it returns an error.
+	// It return an error if the user does not have the role specified in the slice, or the user otherwise.
+	//
+	// Parameters:
+	// - ctx: the context containing the JWT token.
+	// - roles: a slice of roles that are required for access.
+	//
+	// Returns:
+	// - The user if they have the required role.
+	// - An error if the token is missing, invalid, or the user does not have the required role.
 	AllowAccessWithRole(ctx context.Context, roles []role.ROLE) (repository.User, error)
 }
 
@@ -27,18 +54,6 @@ func NewAuthManager(r *repository.UserRepository) *AuthManager {
 	}
 }
 
-// RestrictAccessWithRole restricts access to a user based on their role.
-// It verifies the JWT token from the context and checks if the user has one of the required roles.
-// If the token is missing, invalid, or the user does not have the required role, it returns an error.
-// It return an error if the user have the role specified in the slice or the user otherwise.
-//
-// Parameters:
-// - ctx: the context containing the JWT token.
-// - roles: a slice of roles that are required for access.
-//
-// Returns:
-// - The user if they have the required role.
-// - An error if the token is missing, invalid, or the user does not have the required role.
 func (am *AuthManager) RestrictAccessWithRole(ctx context.Context, roles []role.ROLE) (repository.User, error) {
 	token, _ := ctx.Value(hooks.ServerContextKey("Authorization")).(string)
 	if token == "" {
@@ -71,18 +86,6 @@ func (am *AuthManager) RestrictAccessWithRole(ctx context.Context, roles []role.
 	}
 }
 
-// AllowAccessWithRole allows access to a user based on their role.
-// It verifies the JWT token from the context and checks if the user has one of the required roles.
-// If the token is missing, invalid, or the user does not have the required role, it returns an error.
-// It return an error if the user does not have the role specified in the slice, or the user otherwise.
-//
-// Parameters:
-// - ctx: the context containing the JWT token.
-// - roles: a slice of roles that are required for access.
-//
-// Returns:
-// - The user if they have the required role.
-// - An error if the token is missing, invalid, or the user does not have the required role.
 func (am *AuthManager) AllowAccessWithRole(ctx context.Context, roles []role.ROLE) (repository.User, error) {
 	token, _ := ctx.Value(hooks.ServerContextKey("Authorization")).(string)
 	if token == "" {
