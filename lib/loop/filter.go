34a9d043407a7aca1ceb33dc329669c0247007cd
hhertout <hugues.hertout@gmail.com>
2024-10-21 21:45:26 +0200

    doc: adding doc for loop func

--- lib/loop/filter.go
+++ lib/loop/filter.go
@@ -1,5 +1,22 @@
 package loop
 
+// Filter applies a callback function to each element of an input array and returns a new array with the elements that satisfy the callback condition.
+//
+// Parameters:
+// - array: A slice of elements of type I.
+// - callback: A function that takes an element of type I and returns a boolean. If the callback returns true, the element is included in the result.
+//
+// Returns:
+// - A slice of elements of type I, where each element satisfies the condition specified by the callback function.
+//
+// Example:
+// input := []int{1, 2, 3, 4, 5}
+//
+//	result := Filter(input, func(value int) bool {
+//	    return value%2 == 0
+//	})
+//
+// // result is now []int{2, 4}
 func Filter[I any](array []I, callback func(value I) bool) []I {
 	result := []I{}
 
--- lib/loop/map.go
+++ lib/loop/map.go
@@ -1,5 +1,22 @@
 package loop
 
+// Map applies a callback function to each element of an input array and returns a new array with the results.
+//
+// Parameters:
+// - array: A slice of elements of type I.
+// - callback: A function that takes an element of type I and returns a value of type R.
+//
+// Returns:
+// - A slice of elements of type R, where each element is the result of applying the callback function to the corresponding element of the input array.
+//
+// Example:
+// input := []int{1, 2, 3}
+//
+//	result := Map(input, func(value int) string {
+//	    return strconv.Itoa(value)
+//	})
+//
+// // result is now []string{"1", "2", "3"}
 func Map[I any, R any](array []I, callback func(value I) R) []R {
 	result := []R{}
 
